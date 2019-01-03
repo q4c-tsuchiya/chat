632fe1219c68fb2378b789cf395d8d6369d8d42a
宮川潤 <fcfym630@gmail.com>
2019-01-03 16:10:12 +0900

    webpack-dev-serverの導入

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const App = () => <div>Hello world!</div>
+const App = () => <div>Hello world!!!Hello world!!!</div>
 
 render(
     <App/>,
--- webpack.config.js
+++ webpack.config.js
@@ -27,4 +27,8 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.css'],
   },
+  devServer: {
+    contentBase: __dirname,
+    historyApiFallback: true
+  },
 };
