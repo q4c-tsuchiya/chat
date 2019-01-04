125a33fd6ee33539369f0d530cd2873043184b13
q4c-tsuchiya <tsuchiya@q4c.jp>
2019-01-04 10:11:54 +0900

    firestore sample

--- temp/js/firebase.js
+++ temp/js/firebase.js
@@ -27,6 +27,7 @@ db.settings({
 const collection = db.collection('rooms');
 
 function submit(){
+  console.log('submit here');
   collection.add({
     "roomName": "sample room",
     "chat": {
@@ -35,3 +36,81 @@ function submit(){
     }
   });
 }
+// 以下の参考サイト
+// https://firebase.google.com/docs/firestore/query-data/get-data?hl=ja
+
+function getData(){
+  console.log('getDate here');
+  const got = collection.get()
+    .then(snapshot => {
+        snapshot.forEach(doc => {
+          console.log('doc id:', doc.id);
+          console.log('doc data:', doc.data());
+        })
+    })
+    .catch(err => {
+      console.log('Error getting snapshot', err);
+    });
+  // console.log('got: ', got);
+}
+
+function getDoc(){
+  const roomDocRef = db.collection('rooms').doc('roomA');
+  roomDocRef.get()
+  .then(doc => {
+    console.log('doc: ', doc.data());
+  })
+  .catch(err => {
+    console.log('error!');
+  })
+}
+
+function getSubCollections(){
+  console.log('getSubCollections id:');
+  const roomDocRef = db.collection('rooms').doc('roomA');
+  roomDocRef.collection('messages').get()
+  .then(collections => {
+    collections.forEach(cl => {
+      console.log('cl id: ', cl.id);
+      console.log('cl data: ', cl.data());
+    })
+  })
+  .catch(err => {
+    console.log('error!');
+  })
+}
+
+function searchRoom(){
+  const word = "日本語";
+  const roomRef = db.collection('rooms').doc(word);
+  roomRef.get()
+  .then(doc => {
+    console.log('doc id: ', doc.id);
+    console.log('doc: ', doc.data());
+  })
+  .catch(err => {
+    console.log('error!');
+  })
+}
+
+function getSnapshot() {
+  const roomRef = db.collection('rooms').doc('roomA');
+  roomRef.onSnapshot(snapshot => {
+    console.log('snapshot: ', snapshot.data());
+  }, err => {
+    console.log('error!: ', err);
+  })
+}
+
+function setData() {
+  const roomRef = db.collection('rooms').doc('roomB');
+  roomRef.set({
+    name: "set 1 name",
+    mes: "mes",
+    cl: {
+      incl: "in1",
+      incl2: "in12"
+    }
+  });
+}
+
